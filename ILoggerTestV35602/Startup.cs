d86240f3a46a29adcf9d1024bc2c9d1efd92a957
Matthew Cavallo <matt.cavallo@outlook.com>
2020-10-17 15:19:11 -0500

    Testing using IServiceCollection and logging with a new TelemetryClient in Startup

--- ILoggerTestV35602/Startup.cs
+++ ILoggerTestV35602/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 
 [assembly: FunctionsStartup(typeof(ILoggerTestV35602.Startup))]
 
@@ -12,7 +14,41 @@ namespace ILoggerTestV35602
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddSingleton<IMyLoggerProvider, MyLoggerProvider>();
+            try
+            {
+                ConfigureServices(builder.Services).BuildServiceProvider(true);
+
+                throw new Exception("TEST EXCEPTION!!!");
+            }
+            catch (Exception ex)
+            {
+                var config = new TelemetryConfiguration(Environment.GetEnvironmentVariable("APPINSIGHTS_INSTRUMENTATIONKEY"));
+                var client = new TelemetryClient(config);
+                client.TrackException(ex);
+                client.Flush();
+
+                throw;
+            }
+
+
+
+            //builder.Services.AddSingleton<IMyLoggerProvider, MyLoggerProvider>();
+        }
+
+        private IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMyLoggerProvider, MyLoggerProvider>();
+
+            //services
+            //    .AddLogging(loggingBuilder =>
+            //        loggingBuilder.AddSerilog(dispose: true)
+            //    )
+            //    .AddTransient<IJsonPlaceholderClient, JsonPlaceholderClient>(client =>
+            //        new JsonPlaceholderClient(Environment.GetEnvironmentVariable("BaseAddress"))
+            //    )
+            //    .AddTransient<IJsonPlaceholderService, JsonPlaceholderService>();
+
+            return services;
         }
     }
 }
